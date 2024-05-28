695ac672ec1577dd0dfd37d29bc3fd63f427ed73
RGB-Keys <ramongtstudy@gmail.com>
2024-05-28 15:41:28 -0300

    Finalizando projeto

--- src/main/java/med/voll/api/domain/dtos/medicos/DadosDetalhamentoMedicos.java
+++ src/main/java/med/voll/api/domain/dtos/medicos/DadosDetalhamentoMedicos.java
@@ -3,8 +3,8 @@ package med.voll.api.domain.dtos.medicos;
 import med.voll.api.domain.models.Endereco;
 import med.voll.api.domain.models.Medico;
 
-public record DadosDetalhamentoMedicos(String nome, String email, String telefone, String crm, Especialidade especialidade, Endereco endereco) {
+public record DadosDetalhamentoMedicos(Long id, String nome, String email, String telefone, String crm, Especialidade especialidade, Endereco endereco) {
     public DadosDetalhamentoMedicos(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
     }
 }
---
+++ src/test/java/med/voll/api/controllers/MedicoControllerTest.java
@@ -0,0 +1,104 @@
+package med.voll.api.controllers;
+
+import med.voll.api.domain.dtos.enderecos.DadosEnderecos;
+import med.voll.api.domain.dtos.medicos.DadosCadastroMedicos;
+import med.voll.api.domain.dtos.medicos.DadosDetalhamentoMedicos;
+import med.voll.api.domain.dtos.medicos.Especialidade;
+import med.voll.api.domain.models.Endereco;
+import med.voll.api.domain.models.Medico;
+import med.voll.api.domain.repositories.MedicoRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class MedicoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosCadastroMedicos> dadosCadastroMedicoJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoMedicos> dadosDetalhamentoMedicoJson;
+
+    @MockBean
+    private MedicoRepository repository;
+
+    @Test
+    @DisplayName("Deveria devolver codigo http 400 quando informacoes estao invalidas")
+    @WithMockUser
+    void cadastrar_cenario1() throws Exception {
+        var response = mvc
+                .perform(post("/medicos"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deveria devolver codigo http 200 quando informacoes estao validas")
+    @WithMockUser
+    void cadastrar_cenario2() throws Exception {
+        var dadosCadastro = new DadosCadastroMedicos(
+                "Medico",
+                "medico@voll.med",
+                "61999999999",
+                "123456",
+                Especialidade.CARDIOLOGIA,
+                dadosEndereco());
+
+        when(repository.save(any())).thenReturn(new Medico(dadosCadastro));
+
+        var response = mvc
+                .perform(post("/medicos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dadosCadastroMedicoJson.write(dadosCadastro).getJson()))
+                .andReturn().getResponse();
+
+        var dadosDetalhamento = new DadosDetalhamentoMedicos(
+                null,
+                dadosCadastro.nome(),
+                dadosCadastro.email(),
+                dadosCadastro.telefone(),
+                dadosCadastro.crm(),
+                dadosCadastro.especialidade(),
+                new Endereco(dadosCadastro.endereco())
+        );
+        var jsonEsperado = dadosDetalhamentoMedicoJson.write(dadosDetalhamento).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+    }
+
+    private DadosEnderecos dadosEndereco() {
+        return new DadosEnderecos(
+                "rua xpto",
+                "bairro",
+                "00000000",
+                "Brasilia",
+                "DF",
+                null,
+                null
+        );
+    }
+
+}
